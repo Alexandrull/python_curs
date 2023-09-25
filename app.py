76ba879a03fbf592d98d90fa6665856b77b7d111
Alexandrull <ghitaalexandru987@gmail.com>
2023-09-25 09:24:25 +0300

    1st_HackerRank

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 if __name__ == '__main__':
+    count = 0
     for x in range(1, 10):
         if x % 2 == 0:
+            count += 1
             print(x)
 
-print("We have 4 even numbers")
+print(f"We have  even numbers")
